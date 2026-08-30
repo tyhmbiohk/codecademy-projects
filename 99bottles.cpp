74fba029193687bec9f74c4439304d52fe2c03b8
tyhmbiohk <coolzyq@gmail.com>
2026-08-30 15:00:32 +0800

    Add files via upload

---
+++ 99bottles.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+      
+    int i;
+    for (i=99; i>0; i--){
+    std::cout<<i<<"    bottles of pop on the wall. Take one down and pass it around." << i-1<< "bottles of pop on the wall.\n";
+
+
+  }// Write a for loop here:
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
